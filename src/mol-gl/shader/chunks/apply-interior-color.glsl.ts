41c2a408d9c674a7a0657448bfb42ad3941e29f5
Liuharry2003 <924255397@qq.com>
2022-02-28 11:49:06 +0800

    changed

--- src/mol-gl/shader/chunks/apply-interior-color.glsl.ts
+++
@@ -1,9 +0,0 @@
-export const apply_interior_color = `
-if (interior) {
-    if (uInteriorColorFlag) {
-        gl_FragColor.rgb = uInteriorColor;
-    } else {
-        gl_FragColor.rgb *= 1.0 - uInteriorDarkening;
-    }
-}
-`;
